f5f9c5f7dc3d7cbfd11dc999be74e23b68295be2
Jerrely Vizueta <victorv2718@gmail.com>
2023-11-08 23:10:21 -0500

    Tweaks to Nutrition Func

--- Tasks/NutritionFacts/nutrition.py
+++ Tasks/NutritionFacts/nutrition.py
@@ -5,9 +5,8 @@ import numpy as np
 from keras import models,layers
 
 load_dotenv()
-file = os.environ.get('dataset_location')
-food_set = pd.read_csv(file).fillna(0.0)
-
+FILE = os.environ.get('dataset_location')
+FOOD_SET = pd.read_csv(FILE).fillna(0.0)
 DETERMINANT = 2
 DAILY_VALUES = {'calories': 2000.0,
                 'total_fat': 65.0,
@@ -16,28 +15,19 @@ DAILY_VALUES = {'calories': 2000.0,
                 'sodium':2300.0,
                 'carbohydrate':225.0,
                 'fiber':30.0,
-                'sugar':50.0,
+                'sugars':50.0,
                 'calcium':20.0,
                 'iron': 18.0,
                 'potassium':4700.0}
-DIV = round(0.8 * len(food_set))
+DIV = round(0.8 * len(FOOD_SET))
 
 x, y = [],[]
-for product in food_set.values:
-    current_facts = {'calories': product[3],
-                'total_fat': product[4],
-                'saturated_fat': product[5],
-                'cholesterol':product[6],
-                'sodium':product[7],
-                'carbohydrate':product[58],
-                'fiber':product[59],
-                'sugar':product[60],
-                'calcium':product[29],
-                'iron': product[31],
-                'potassium':product[35]}
-    
+data = FOOD_SET[['calories','total_fat','saturated_fat','cholesterol','sodium','carbohydrate','fiber','sugars','calcium','iron','potassium']]
+
+for current_facts in data.values:
     product_values = []
-    for daily,fact in zip(DAILY_VALUES.values(),current_facts.values):
+
+    for daily,fact in zip(DAILY_VALUES.values(),current_facts):
         percentage = (fact / daily)
         product_values.append(round(percentage,5))
 
@@ -59,8 +49,6 @@ for product in food_set.values:
 x_train,y_train = np.array(x[:DIV]),np.array(y[:DIV])
 x_test,y_test = np.array(x[DIV:]),np.array(y[DIV:])
 
-print(len(x_train),len(y_train))
-print(len(x))
 model = models.Sequential()
 model.add(layers.Dense(128, activation='relu', input_shape=(11,)))
 model.add(layers.Dense(64, activation='relu'))
@@ -70,8 +58,5 @@ model.add(layers.Dense(1, activation='sigmoid'))
 
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-
-
-history = model.fit(x_train,y_train,epochs=50,batch_size=32,validation_data=(x_test,y_test))
-
+history = model.fit(x_train,y_train,epochs=20,batch_size=32,validation_data=(x_test,y_test))
 
