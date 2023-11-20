bb950f840f6352f85cc75e6892fd4edabe26ecbf
Jerrely Vizueta <94874872+Jerrely1618@users.noreply.github.com>
2023-11-20 12:35:57 -0500

    Update nutrition.py
    chore: Trying new parameters on Nutrition model

--- Tasks/NutritionFacts/nutrition.py
+++ Tasks/NutritionFacts/nutrition.py
@@ -3,6 +3,9 @@ import os
 from dotenv import load_dotenv
 import numpy as np
 from keras import models,layers
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 import time
 
 load_dotenv()
@@ -22,6 +25,8 @@ DAILY_VALUES = {'calories': 2000.0,
                 'potassium':4700.0}
 DIV = round(0.8 * len(FOOD_SET))
 
+
+
 def timing_val(func):
     def wrapper(*arg, **kw):
         t1 = time.time()
@@ -32,10 +37,12 @@ def timing_val(func):
 
 @timing_val
 def nutrifact():
-    x, y = [],[]
-    data = FOOD_SET[['calories','total_fat','saturated_fat','cholesterol','sodium','carbohydrate','fiber','sugars','calcium','iron','potassium']]
+    y = []
+    scaler = StandardScaler()
+    x = scaler.fit_transform(FOOD_SET[['calories', 'total_fat', 'saturated_fat', 'cholesterol', 'sodium', 'carbohydrate', 'fiber', 'sugars', 'calcium', 'iron', 'potassium']])
 
-    for current_facts in data.values:
+    
+    for current_facts in x:
         product_values = np.zeros(11,)
 
         for i,(daily,fact) in enumerate(zip(DAILY_VALUES.values(),current_facts)):
@@ -46,10 +53,9 @@ def nutrifact():
 
         y.append(1 if product_unhealthy == DETERMINANT else 0)
 
-        x.append(product_values)
+    y = np.array(y)
 
-    x_train,y_train = np.array(x[:DIV]),np.array(y[:DIV])
-    x_test,y_test = np.array(x[DIV:]),np.array(y[DIV:])
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
     model = models.Sequential()
     model.add(layers.Dense(128, activation='relu', input_shape=(11,)))
@@ -59,8 +65,13 @@ def nutrifact():
     model.add(layers.Dense(1, activation='sigmoid'))
 
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
+    y_pred = model.predict(x_test)
     history = model.fit(x_train,y_train,epochs=5,batch_size=32,validation_data=(x_test,y_test))
+    test_loss, test_accuracy = model.evaluate(x_test, y_test)
+    conf_matrix = confusion_matrix(y_test, y_pred)
+    print(f'Test Accuracy: {test_accuracy * 100:.2f}%')
+    print("Confusion Matrix:")
+    print(conf_matrix)
     return "Done"
 
 print(nutrifact())
